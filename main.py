e7eba0f2195f108b2da68103d42b91838b9b14d3
RodrigoUngo <00075419@uca.edu.sv>
2024-10-11 16:50:47 +0000

    Test commit #7

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ def hello_http(request):
     elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
-        name = 'World: Testing the trigger with a push (Test 6)'
+        name = 'World: Testing the trigger with a push (Test 7)'
     return 'Hello {}!'.format(name)
